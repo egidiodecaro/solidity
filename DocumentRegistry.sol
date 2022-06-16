76dceb512dcf2681da474ca66678640f23184d5f
egidiodecaro <65916647+egidiodecaro@users.noreply.github.com>
2022-06-16 09:16:34 +0200

    Contract revision

--- DocumentRegistry.sol
+++ DocumentRegistry.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.6;
+pragma solidity >0.5.6;
+pragma experimental ABIEncoderV2;
 
 /* 
  * DocumentRegistry contract by eg
@@ -21,94 +22,109 @@ contract DocumentRegistry{
 
     event Registered(address indexed _signer, bytes32 _protocol, uint _date);
     event Revisioned(address indexed _signer, bytes32 _protocol, uint _date);
-    event DocumentNotPresent(address indexed _signer, bytes32 _protocol, uint _date);
-    event DocumentAlreadyPresent(address indexed _signer, bytes32 _protocol, uint _date);
-    event ExceptionEvent(address indexed _signer, string message , uint date);
 
     constructor() public{
         owner = msg.sender;
     }
 
-     function isRegistered(bytes32 _hash)
-    public view returns (bool)
-    {
-        return (registry[_hash].documentHash == _hash);
+    modifier registered(bytes32 _hash){
+        require(isRegistered(_hash) , "Document not registered;");
+        _;
+    }
+    modifier notRegistered(bytes32 _hash){
+        require(!isRegistered(_hash) , "Document already registered;");
+        _;
+    }
+    modifier diverseInputs(bytes32 _newHash , bytes32 _previousHash){
+        require(_newHash != _previousHash , "The revisioned document is equal to the old one");
+        _;
     }
 
-    function registerDocument(bytes32 _hash, string calldata _metadata) external {
-        uint date = now;
+    modifier oldRegisteredNewUnregistered(bytes32 _newHash,bytes32 _previousHash){
+        require(isRegistered(_previousHash) && !isRegistered(_newHash) , "Document is not present or the revision it's been already registered");
+        _;
+    }
 
-        if(!isRegistered(_hash)){
+    function isRegistered(bytes32 _hash)
+        public view returns (bool)
+        {
+            return (registry[_hash].documentHash == _hash);
+        }
+
+    function isRevisioned(bytes32 _protocol)
+        public view returns (bool){
+            return (registry[_protocol].newHash != _protocol);
+        }
+
+    function registerDocument(bytes32 _hash, string calldata _metadata) external notRegistered(_hash){
             registry[_hash].signer = msg.sender;
             registry[_hash].documentHash = _hash;
-            registry[_hash].date = date;
+            registry[_hash].date = block.timestamp;
             registry[_hash].previousHash = _hash;
             registry[_hash].revisionNumber = 1;
             registry[_hash].newHash = _hash;
             registry[_hash].jsonMetadata = _metadata;
-            emit Registered(msg.sender , _hash , date);
-        }
-        else emit DocumentAlreadyPresent(msg.sender,  _hash , date);
+            emit Registered(msg.sender , _hash , block.timestamp);
     }
 
-    function registerDocumentRevision(bytes32 _newHash , bytes32 _previousHash) external {
-        uint date = now;
-
-        if(_newHash == _previousHash){
-            emit ExceptionEvent(msg.sender , "the revisioned document is equal to the old one" , date);
-            return;
-        }
-
-        if(isRegistered(_previousHash) && !isRegistered(_newHash)){
+    function registerDocumentRevision(bytes32 _newHash , bytes32 _previousHash) external
+    diverseInputs(_newHash,_previousHash) oldRegisteredNewUnregistered(_newHash,_previousHash){        
             /*inserisco nuova versione documento*/
             registry[_newHash].signer = msg.sender;
             registry[_newHash].documentHash = _newHash;
-            registry[_newHash].date = date;
+            registry[_newHash].date = block.timestamp;
             registry[_newHash].previousHash = _previousHash;
             registry[_newHash].revisionNumber = registry[_previousHash].revisionNumber + 1;
             registry[_newHash].newHash = _newHash;
             registry[_newHash].jsonMetadata = registry[_previousHash].jsonMetadata;
-            emit Registered(msg.sender , _newHash , date);
+            emit Registered(msg.sender , _newHash , block.timestamp);
 
             /*aggiorno riferimenti versione precedente documento*/
             registry[_previousHash].newHash = _newHash;
-            emit Revisioned(msg.sender , _previousHash , date);
+            emit Revisioned(msg.sender , _previousHash , block.timestamp);
+    }    
 
-        }
-        else emit DocumentNotPresent(msg.sender , _previousHash , date);
-    }
-
-    function isRevisioned(bytes32 _protocol)
-    public view returns (bool)
-    {
-        return (registry[_protocol].newHash != _protocol);
-    }
-
-    function getLatestRevision(bytes32 _hash) public returns (bytes32){
-        if(isRegistered(_hash)){
-            bytes32 latestRev = registry[_hash].documentHash;
-            while(registry[latestRev].documentHash != registry[latestRev].newHash){
-                
-                latestRev = registry[latestRev].newHash;
+    function getLatestRevision(bytes32 _hash) public view
+    registered(_hash) returns (bytes32){
+        bytes32 latestRev = registry[_hash].documentHash;
+        while(registry[latestRev].documentHash != registry[latestRev].newHash){
+            latestRev = registry[latestRev].newHash;
             }
         return latestRev;
-        }
-        else
-        emit DocumentNotPresent(msg.sender , _hash , now);
-        return "";
     }
 
-    function getRevisionHistory(bytes32 _hash) external returns (bytes32[] memory){
-        if(registry[_hash].documentHash == _hash){
-            bytes32 latestRevision = getLatestRevision(_hash);
-            uint n = registry[latestRevision].revisionNumber;
-            bytes32[] memory history = new bytes32[](n);
-            for(uint i = n; i >= 1 ; i--){
-                history[i-1] = registry[latestRevision].documentHash;
-                latestRevision = registry[latestRevision].previousHash;
-                }
-            return history;
+    function getFullRevisionHistory(bytes32 _hash) external view 
+    registered(_hash) returns (bytes32[] memory){
+        bytes32 latestRevision = getLatestRevision(_hash);
+        uint n = registry[latestRevision].revisionNumber;
+        bytes32[] memory history = new bytes32[](n);
+        for(uint i = n; i >= 1 ; i--){
+            history[i-1] = registry[latestRevision].documentHash;
+            latestRevision = registry[latestRevision].previousHash;
+            }
+        return history;
+    }
+
+    function getPartialRevisionHistory(bytes32 _hash) external view 
+    registered(_hash) returns (bytes32[] memory){
+        bytes32 tmp = _hash;
+        uint n = registry[tmp].revisionNumber;
+        bytes32[] memory history = new bytes32[](n);
+        for(uint i = n; i >= 1 ; i--){
+            history[i-1] = registry[tmp].documentHash;
+            tmp = registry[tmp].previousHash;
+            }
+        return history;
+    }
+
+    function getDocument(bytes32 _hash) registered(_hash) external view returns(OutputDocument memory outputDocument){
+            return OutputDocument(registry[_hash].documentHash , registry[_hash].date , registry[_hash].jsonMetadata, registry[_hash].revisionNumber);
         }
-        else emit DocumentNotPresent(msg.sender , _hash , now);
+
+    struct OutputDocument{
+        bytes32 _hash;
+        uint date;
+        string jsonMetadata;
+        uint revisionNumber;
     }
 }
